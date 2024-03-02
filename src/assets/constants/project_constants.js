9fdea3db12e3b08b484a6bf71e926410cfbefe65
saishankar47 <saishankarvanam@gmail.com>
2024-03-02 12:29:18 -0500

    Updated the Project

--- src/assets/constants/project_constants.js
+++ src/assets/constants/project_constants.js
@@ -1,23 +1,40 @@
-export const project_constants = [
-    {
-        "title" : "Exam Study Tool",
-        "description" : "Exam Study Tool is a dynamic and user-friendly web application designed to facilitate comprehensive exam preparation for students across diverse linguistic backgrounds. With its intuitive interface, students can access a vast repository of questions in multiple languages, enabling them to enhance their understanding and proficiency in various subjects. Moreover, teachers benefit from the platform's robust features, which allow them to curate tailored tests and assessments suited to the individual needs of their students, all within a multilingual framework. This innovative tool not only fosters a conducive learning environment but also promotes inclusivity and accessibility in education.",
-        "technologies_used" : ["EJS", "NodeJs", "HTML","Bootstrap","MySQL","AWS"],
-        "code_link" : "https://github.com/saishankar47/Examstudytool.git",
-        "project_image" : "EST.png"
-    },
-    {
-        "title" : "Face Recognition using Similarity Feature-Based Selection and Classification",
-        "description" : "Developed automated threshold selection for face recognition using SFSC algorithms and wrapper models. Improved accuracy by selecting optimal feature sets. Tested on ORL and Georgia Tech face databases, yielding superior results.",
-        "technologies_used" : ["Python", "TensorFlow", "OpenCV"],
-        "code_link" : "",
-        "project_image" : "FR1.png"
-    },
-    {
-        "title" : "Coupon-X",
-        "description" : "The idea was about exchange of coupons on an E-Commerce website. We developed an E-Commerce website along with the feature of trading Coupons",
-        "technologies_used" : ["HTML", "CSS", "Bootsrap 4", "JQuery", "MySQL", "JSP"],
-        "code_link" : "https://github.com/saishankar47/Coupon-X.git",
-        "project_image" : "coupon-x.jpg"
-    }
-]
+
+    export const project_constants = [
+        {
+            "title" : "Exam Study Tool",
+            "description" : "Exam Study Tool is a dynamic and user-friendly web application designed to facilitate comprehensive exam preparation for students across diverse linguistic backgrounds. With its intuitive interface, students can access a vast repository of questions in multiple languages, enabling them to enhance their understanding and proficiency in various subjects. Moreover, teachers benefit from the platform's robust features, which allow them to curate tailored tests and assessments suited to the individual needs of their students, all within a multilingual framework. This innovative tool not only fosters a conducive learning environment but also promotes inclusivity and accessibility in education.",
+            "technologies_used" : ["EJS", "NodeJs", "HTML","Bootstrap","MySQL","AWS"],
+            "code_link" : "https://github.com/saishankar47/Examstudytool.git",
+            "project_image" : "EST.png"
+        },
+        {
+            "title" : "Online Appointment System",
+            "description" : "Developed a website facilitating patient appointments with specialized doctors, offering comprehensive patient records including medical history and genetic predispositions. Implemented features to notify patients of doctor unavailability, enhancing scheduling efficiency. Utilized REST API and Spring MVC for seamless functionality.",
+            "technologies_used" : ["Spring MVC", "HTML5", "CSS3", "JavaScript", "jQuery", "Bootstrap", "Ajax", "JSON", "Hibernate", "Native SQL"],
+            "code_link" : "https://github.com/saishankar47/smart-covid-clinic-front-end",
+            "project_image" : "online_appointment.jpeg"
+        },
+        {
+            "title" : "Coupon-X",
+            "description" : "The idea was about exchange of coupons on an E-Commerce website. We developed an E-Commerce website along with the feature of trading Coupons",
+            "technologies_used" : ["HTML", "CSS", "Bootsrap 4", "JQuery", "MySQL", "JSP"],
+            "code_link" : "https://github.com/saishankar47/Coupon-X.git",
+            "project_image" : "coupon-x.jpg"
+        },
+        {
+            "title" : "Face Recognition using Similarity Feature-Based Selection and Classification",
+            "description" : "Developed automated threshold selection for face recognition using SFSC algorithms and wrapper models. Improved accuracy by selecting optimal feature sets. Tested on ORL and Georgia Tech face databases, yielding superior results.",
+            "technologies_used" : ["Python", "TensorFlow", "OpenCV"],
+            "code_link" : "",
+            "project_image" : "FR1.png"
+        },
+        {
+            "title" : "Smart Alert System for Fast Moving Vehicles using Wireless Sensor Networks",
+            "description" : "Implemented a student vehicle monitoring system using Arduino, RFID, and GSM technology. Parents can control vehicle speed; if exceeding 60 KMPH, the alert system notifies parents to take action. Ensures safety by preventing accidents, with exemptions for emergency vehicles. This approach promotes fuel efficiency and reduces air pollution while involving parents in speed control.",
+            "technologies_used" : ["C", "IoT", "Embedded Systems"],
+            "code_link" : "",
+            "project_image" : "IOT.jpg"
+        },
+        
+    ]
+    // 
