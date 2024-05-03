a579f8a76dc9337cd375783c9d1cda2e4329635f
18211a12a2 <71053336+18211a12a2@users.noreply.github.com>
2024-05-02 22:32:00 -0500

    Update project_constants.js

--- src/assets/constants/project_constants.js
+++ src/assets/constants/project_constants.js
@@ -5,14 +5,14 @@ export const project_constants = [
         "title" : "Facial Identification for Missing Child",
         "description" : "Spearheaded the development of a facial recognition classifier using the K-Nearest Neighbors algorithm, achieving a groundbreaking 65% accuracy in identifying missing children. Spearheaded the development of a user-friendly web and mobile app, enabling seamless data management and match updates reduced identification process time by 30% and enhanced user experience resulting in a 20% increase in user retention. Deployed the system on scalable cloud infrastructure, handling a 50% increase in traffic demands while ensuring 99% uptime and sub-300ms response times, maintaining high availability and responsiveness. Contributed to over 1,000 missing children cases, pivotal in elevating success rates for reuniting families in distress.",
         "technologies_used" : [ "Python", "FastAPI", "scikit-learn" , "OpenCV", "NumPy", "Pandas",  "Pillow (PIL)"],
-        "code_link" : "",
+        "code_link" : "https://github.com/18211a12a2/Facial-Identification-for-Missing-Child.git",
         "project_image" : "fimc.jpeg"
     },
     {
         "title" : "Toxic Comment Classification",
         "description" : "Engineered innovative toxic comment classification system with Naive Bayes, SVM, and ensemble techniques, achieving 80.72% F1 score and 74% recall rate, enhancing online community moderation. Implemented advanced NLP techniques, including TF-IDF vectorization, and optimized feature engineering, resulting in an 11% performance gain over benchmark model, fostering a positive online environment while minimizing overhead. Evaluated 10+ machine learning algorithms like logistic regression, decision trees, and ensemble methods for toxic comment classification, deploying a robust, scalable LightGBM solution achieving 85% accuracy in safeguarding online interactions with minimal false positives, showcasing expertise in data analytics and risk mitigation.",
         "technologies_used" : [ "Python", "Scikit-learn", "NumPy", "Pandas", "Dlib", "NLP", "Logistic Regression", "Naive Bayes", "SVM", "LightGBM"],
-        "code_link" : "",
+        "code_link" : "https://github.com/18211a12a2/Toxic-Comment-Classification.git",
         "project_image" : "tcc.png"
     },
 
@@ -20,14 +20,14 @@ export const project_constants = [
         "title" : "PPS-R18",
         "description" : "Developed an innovative Android app, ’PPS-R18,” using Android Studio and Java to provide interactive C programming lessons and exercises. In line with the R-18 syllabus, the offline interface of the app enabled self-paced learning, leading to a 35% improvement in programming skills for 550 students.",
         "technologies_used" : ["Android Studio", "Java", "XML"],
-        "code_link" : "",
+        "code_link" : "https://github.com/18211a12a2/PPS-R18.git",
         "project_image" : "clang.png"
     },
     {
         "title" : "Credit Card Fraud Detection",
         "description" : "Designed and implemented a robust credit card fraud detection system by integrating decision tree methodology with Luhn's and Hunt's algorithms, resulting in a 70% increase in fraud detection accuracy and a 40% reduction in false positives. Demonstrated proficiency in advanced data analytics and risk management techniques while contributing to the safeguarding of financial assets",
         "technologies_used" : ["Python", "TensorFlow", "OpenCV"],
-        "code_link" : "",
+        "code_link" : "https://github.com/18211a12a2/CREDIT-CARD-FRAUD-DETECTION.git",
         "project_image" : "ccfd.png"
     },
 ]
